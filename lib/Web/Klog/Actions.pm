fa9ad58618d82b03a022de2f1faeaaee547242a1
Yaroslav Korshak <ykorshak@gmail.com>
2011-08-13 12:51:02 +0000

    web: utf8, charset header, navigation start. all in one. sorry

--- lib/Web/Klog/Actions.pm
+++ lib/Web/Klog/Actions.pm
@@ -16,14 +16,27 @@ sub index {
     my $self = shift;
     my ($env) = @_;
 
-    my $start = $env->param('skip');
-    my $chan = $env->param('chan') || '#kiev.pm';
+    $self->{prev_nick} = '';
+
+
+    my $self->{start} = $env->param('skip');
+
+    if (!defined($self->{start}) || $self->{start} !~ /^\d+$/) {
+        $self->{start} = 0;
+    }
+
+    my $chan = $env->param('chan') || '#ru.pm';
     $chan =~ s/\./_/g;
 
     my $min  = -1;
-    my $show = 40;
+    $self->{show} = 40;
 
-    foreach (split(',', $env->param('hl'))) {
+    my @hl_params;
+    if (my $hl = $env->param('hl')) {
+        @hl_params = split(',', $hl);
+    }
+
+    foreach (@hl_params) {
         if (/^(\d+)\.\.(\d+)$/) {
             for ($1 .. $2) {
                 $self->{hl}{$_} = 1;
@@ -37,40 +50,40 @@ sub index {
     }
 
     my $db = DBI->connect('DBI:mysql:database=irc_log;host=127.0.0.1',
-        undef, undef, {mysql_enable_utf8 => 1})
+        undef, undef, {mysql_auto_reconnect => 1, mysql_enable_utf8 => 1})
       or die;
 
-    $start =~ /^\d+$/ or $start = 0;
 
     my ($tbl) = $db->selectrow_array(
         'SHOW TABLE STATUS WHERE NAME = ' . $db->quote($chan . "_log"));
 
-
     unless ($tbl) {
-        print "This channel was never logged: $chan\n";
-        exit(0);
+        my $body = $self->render('index',
+            body => "This channel was never logged: $chan");
+        return [200, [], [$body]];
     }
+
 
     my ($count) = $db->selectrow_array('SELECT COUNT(*) FROM `' . $tbl . '`');
 
     if ($min >= 0) {
-        ($start) =
-          $db->selectrow_array('SELECT COUNT(*) FROM `'
+        ($self->{start}) =
+          $db->selectrow_array('SELECT COUNT(*) FROM `' 
               . $tbl
               . '` WHERE id > '
-              . ($min + $show - 3));
+              . ($min + $self->{show} - 3));
     }
 
-    if ($start > $count - $show) {
-        $start = $count - $show;
-        if ($start < 0) { $start = 0 }
+    if ($self->{start} > $count - $self->{show}) {
+        $self->{start} = $count - $self->{show};
+        if ($self->{start} < 0) { $self->{start} = 0 }
     }
 
     my $data = $db->selectall_arrayref(
-        'SELECT *, unix_timestamp(`time`) AS time_unix  FROM `'
+        'SELECT *, unix_timestamp(`time`) AS time_unix  FROM `' 
           . $tbl
           . '` ORDER BY `time` DESC, id DESC LIMIT ?, ?',
-        {Slice => {}}, $start, $show
+        {Slice => {}}, $self->{start}, $self->{show}
     );
     my $body;
 
@@ -78,13 +91,39 @@ sub index {
         $body .= $self->render_message($row);
     }
 
-    $body = $self->render('index', body => $body);
+    my $need_navbar;
 
-    if (Encode::is_utf8($body)) {
-        $body = Encode::encode('UTF-8', $body);
+    my $nav_param = {};
+    if ($self->{start} > 0) {
+        $nav_param->{next} = $self->{start} > $sself->{how} ? $self->{start} - $self->{show} : 0;
+        $need_navbar++;
     }
 
-    [200, ['Content-Type' => 'text/html'], [$body,]];
+    if ($count > $self->{start} +  $self->{show}) {
+        $nav_param->{back} = $self->{start} + $self->{show});
+        $need_navbar++;
+    }
+    if ($need_navbar) {
+        $nav_param->{pos} = $self->{start};
+    }
+
+    my $navbar =
+      $need_navbar ? $self->render_template('navbar', %$nav_param) : '';
+
+    $body = $self->render('index', body => $body, navbar => $navbar);
+
+    my $content_type = 'text/html';
+    if (Encode::is_utf8($body)) {
+        $body = Encode::encode('UTF-8', $body);
+        $content_type .= '; charset=utf-8'
+          unless $content_type =~ /;\s*charset=/;
+    }
+
+    [200, ['Content-Type' => $content_type], [$body]];
+}
+
+sub render_navbar {
+    my $
 }
 
 sub render_message {
